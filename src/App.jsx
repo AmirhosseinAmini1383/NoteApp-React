ea7fbe520596e2e3da1c97a85a4e1e0677a60885
AmirhosseinAmini1383 <amirhossein.amini.ghahan83@gmail.com>
2024-11-06 19:53:35 +0330

    useReducer NoteApp

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,42 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import "./App.css";
 import AddNewNote from "./components/AddNewNote";
 import NoteList from "./components/NoteList";
 import NoteStatus from "./components/NoteStatus";
 import NoteHeader from "./components/NoteHeader";
+
+const initialState = [];
+function notesReducer(state, { type, payload }) {
+  switch (type) {
+    case "ADD_NOTE": {
+      return [...state, payload];
+    }
+    case "DELETE_NOTE": {
+      return state.filter((n) => n.id !== payload);
+    }
+    case "COMPLETE_NOTE": {
+      return state.map((note) =>
+        note.id === payload ? { ...note, completed: !note.completed } : note
+      );
+    }
+    default:
+      throw new Error("unknown Action " + type);
+  }
+}
 function App() {
-  const [notes, setNotes] = useState([]);
+  // const [notes, setNotes] = useState([]);
   const [sortBy, setSortBy] = useState("latest");
+  const [notes, dispatch] = useReducer(notesReducer, initialState);
 
   const handleAddNotes = (newNote) => {
-    setNotes((prevNote) => [...prevNote, newNote]);
+    dispatch({ type: "ADD_NOTE", payload: newNote });
   };
   const handleDeleteNote = (id) => {
-    // const filteredNotes = notes.filter((n) => n.id !== id);
-    // setNotes(filteredNotes);
-    setNotes((prevNotes) => prevNotes.filter((n) => n.id !== id));
+    dispatch({ type: "DELETE_NOTE", payload: id });
   };
   const handleCompleteNote = (e) => {
     const noteId = +e.target.value;
-    // const newNotes = notes.map((note) =>
-    //   note.id === noteId ? { ...note, completed: !note.completed } : note
-    // );
-    // setNotes(newNotes);
-    setNotes((prevNotes) =>
-      prevNotes.map((note) =>
-        note.id === noteId ? { ...note, completed: !note.completed } : note
-      )
-    );
+    dispatch({ type: "COMPLETE_NOTE", payload: noteId });
   };
 
   return (
